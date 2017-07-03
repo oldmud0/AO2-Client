e43e5bc0f2938745b6b15665313de72bbca80cd0
oldmud0 <oldmud0@users.noreply.github.com>
2017-07-03 16:14:25 -0500

    Fix YAML error by offloading BASS script to cmd file

---
+++ acquire_bass.cmd
@@ -0,0 +1,10 @@
+:
+:Acquire BASS
+:
+
+curl --verbose -H "Referer: https://www.un4seen.com/bass.html" -H "Host: www.un4seen.com" -o bass.zip https://www.un4seen.com/files/bass24.zip
+md bass
+cd bass
+7z x -y ..\bass.zip >nul
+copy bass.dll c\bass.lib ..
+cd..
